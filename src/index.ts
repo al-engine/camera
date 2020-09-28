14b59489710b0da5a479b2a58afa8466671e21aa
zhesha <savzhe@gmail.com>
2020-09-28 18:11:51 +0300

    save data to buffer

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,49 @@
-import {OrgbValue, Pixels} from "core";
-
-function revertY(y: number, height: number): number {
-  return height - y;
-}
+import {OrgbValue, Pixels, SetPixel} from "core";
 
 export type MoveCamera = (_x: number, _y: number) => void;
 export interface CameraResult {
-  moveCamera: MoveCamera,
-  pixels: Pixels
+  moveCamera: MoveCamera;
+  pixels: Pixels;
+  draw: (setPixel: SetPixel) => void;
+  reset: () => void;
 }
 
-export default function createCamera(height: number) {
+export default function createCamera(height: number, width: number): CameraResult {
   let x = 0, y = 0;
-  return function (pixels: Pixels): CameraResult {
+  let buffer = Array<OrgbValue>();
     function moveCamera(_x: number, _y: number) {
       x = _x;
       y = _y;
     }
-    function setPixel(_x: number, _y: number, color: OrgbValue) {
-      pixels.setPixel(_x - x, revertY(_y - y, height), color);
+    function setPixel(px: number, py: number, color: OrgbValue) {
+      const rx = px - x;
+      const ry = py - y;
+      if (rx >= 0 && rx < width && ry >= 0 && ry < height) {
+        buffer[rx + width * ry] = color;
+      }
     }
-    function getPixel(_x: number, _y: number): OrgbValue {
-      return pixels.getPixel(_x - x, revertY(_y - y, height));
+    function getPixel(px: number, py: number): OrgbValue | void {
+      const rx = px - x;
+      const ry = py - y;
+      if (rx >= 0 && rx < width && ry >= 0 && ry < height) {
+        return buffer[rx + width * ry];
+      }
     }
-    return {moveCamera, pixels: {setPixel, getPixel}};
-  }
+    function draw(setPixel: SetPixel) {
+      for (let i = 0; i < width * height; i++) {
+        const row = Math.floor(i / width);
+        if (buffer[i]) {
+          setPixel(i - row * width, height - row - 1, buffer[i]);
+        } else {
+          setPixel(i - row * width, height - row - 1, 0);
+        }
+      }
+    }
+
+    function reset() {
+      buffer = Array<OrgbValue>();
+    }
+
+    return {moveCamera, draw, reset, pixels: {setPixel, getPixel}};
+
 }
